394838a96a8bee6e7c5bf13523910c936485295d
cwpark410 <tonipark76@gmail.com>
2022-03-17 04:08:14 +0900

    initial

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1 align="center">Welcome to bye-biden 👋</h1>
+<p>
+</p>
+
+> time left until Joe Biden goes home
+
+## Author
+
+👤 **Tony Park**
+
+* Github: [@tonypark76](https://github.com/tonypark76)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ main.js
@@ -0,0 +1,31 @@
+const clockTitle = document.querySelector("#clock");
+
+function tillFreedom() {
+  const currentYear = new Date().getFullYear();
+  const currentHour = new Date().getHours();
+  const currentMin = new Date().getMinutes();
+  const currentSec = new Date().getSeconds();
+
+  const now = new Date();
+  const byeBiden = new Date("2025-01-20");
+
+  const day = String(Math.floor((byeBiden - now) / (1000 * 60 * 60 * 24)) - 1);
+  const hour = shapeTime(24, currentHour);
+  const minute = shapeTime(60, currentMin);
+  const second = shapeTime(60, currentSec);
+
+  clockTitle.innerText = `${day}d ${hour}h ${minute}m ${second}s`;
+}
+
+function shapeTime(maxNum, value) {
+  const leftTime = maxNum - value - 1;
+
+  if (leftTime === maxNum) {
+    return "00";
+  } else {
+    return String(leftTime).padStart(2, "0");
+  }
+}
+
+tillFreedom();
+setInterval(tillFreedom, 1000);
